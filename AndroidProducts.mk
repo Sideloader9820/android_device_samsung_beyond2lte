343c58a54ed690fbe9c40ad7f47e02961b7fdd22
Sideloader9820 <160022449+Sideloader9820@users.noreply.github.com>
2024-02-17 20:42:07 +0000

    AndroidProducts: adapt to derpfest

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beyond2lte.mk
+    $(LOCAL_DIR)/derp_beyond2lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beyond2lte-eng \
-    lineage_beyond2lte-user \
-    lineage_beyond2lte-userdebug
+    derp_beyond2lte-eng \
+    derp_beyond2lte-user \
+    derp_beyond2lte-userdebug
